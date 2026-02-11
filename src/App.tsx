be577a09bc3ec6039f2626fe88d188b38794bf14
Karim Youssef <your@email.com>
2026-02-10 22:07:26 -0500

    pushing

--- src/App.tsx
+++ src/App.tsx
@@ -59,7 +59,7 @@ function App() {
       <header className="header">
         <div className="header-container">
           <div className="logo" onClick={() => setCurrentPage('home')} style={{ cursor: 'pointer' }}>
-            <img src="/newLogo.png" alt="Grounds to Grow" className="logo-img" />
+            <img src="newLogo.png" alt="Grounds to Grow" className="logo-img" />
           </div>
           
           <nav className="nav">
